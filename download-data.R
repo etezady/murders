a9f5e183fc992ffecbaf70c202921c9e37ba0e98
Etezady <etezady@gmail.com>
2019-05-10 02:58:54 -0400

    all files to upstream repo.

---
+++ download-data.R
@@ -0,0 +1,6 @@
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+destfile <- "data/murders.csv"
+download.file(url,destfile)
+
+
+
---
+++ download-reportRmd.R
@@ -0,0 +1,3 @@
+url <- "https://raw.githubusercontent.com/rairizarry/murders/master/report.Rmd"
+destfile <- "report.Rmd"
+download.file(url,destfile)
